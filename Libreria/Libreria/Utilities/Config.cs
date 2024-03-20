7fd41a9961adfe563d312ff5a1dd61c2573c8b79
andreas ubertini <108841328+andreas94ubertini@users.noreply.github.com>
2024-03-20 17:50:54 +0100

    First Commit

---
+++ Libreria/Libreria/Utilities/Config.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria.Utilities
+{
+    internal class Config
+    {
+        private static Config? istanza;
+        private string? connectionString;
+
+        public static Config getIstanza()
+        {
+            if (istanza == null)
+                istanza = new Config();
+
+            return istanza;
+        }
+
+        private Config()
+        {
+
+        }
+        public string? GetConnectionString()
+        {
+            if (connectionString == null)
+            {
+                ConfigurationBuilder builder = new ConfigurationBuilder();
+                builder.SetBasePath(Directory.GetCurrentDirectory());
+                builder.AddJsonFile("appSettings.json", optional: false, reloadOnChange: false);
+
+                IConfiguration configuration = builder.Build();
+
+                connectionString = configuration.GetConnectionString("ServerLocale");
+            }
+
+            return connectionString;
+        }
+    }
+}
